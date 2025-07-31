f2052283a3b9e7fb37660cdb1c11c44be47a7b5b
Arachn1d <isaacwinters3000@gmail.com>
2025-07-30 18:40:17 -0700

    final commit

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -3,32 +3,32 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms.v2 as transforms
 from PIL import Image
+import numpy
 
 MODEL_PATH = "model/model.pth"
     
 class ASLCNN(nn.Module):
-    def __init__(self, num_classes=26):  # change to match your dataset
+    def __init__(self, num_classes=26):
         super(ASLCNN, self).__init__()
 
         self.conv1 = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.ReLU(),
-            nn.MaxPool2d(2))  # 256 → 128
+            nn.MaxPool2d(2))
 
         self.conv2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2))  # 128 → 64
+            nn.MaxPool2d(2))
 
         self.conv3 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2))  # 64 → 32
+            nn.MaxPool2d(2))
 
-        # Calculate flattened size: 128 channels × 32 × 32
         self.fc1 = nn.Linear(128 * 32 * 32, 256)
         self.fc2 = nn.Linear(256, num_classes)
 
@@ -37,7 +37,7 @@ class ASLCNN(nn.Module):
         x = self.conv2(x)
         x = self.conv3(x)
 
-        x = x.view(x.size(0), -1)  # Flatten
+        x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x
@@ -57,8 +57,8 @@ class ImageClassifier():
             transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
         ])
 
-    def classifyImage(self, img_path):
-        image = Image.open(img_path)
+    def predict(self, image):
+        image = image.convert("RGB")
         image = self.testtransform(image)
         image = image.to(self.device)
 
--- README.md
+++ README.md
@@ -3,7 +3,4 @@ A model which can identify the letters of the ASL alphabet.
 
 For now can only consistently identify images taken by a specific person because we didn't have the computational power to train on as wide variety of data as we would have liked. Until we retrain the model with much more data, it is recommended to only try it out on the images provided in the /static/uploads folder.
 
-Uses the API linked here:
-https://huggingface.co/spaces/IWinters/ASL-CNN-API
-
 Isaac Winters, Arnav Agrawal, Yunchan Ji, Ronin Phan, Aaron Kang.
